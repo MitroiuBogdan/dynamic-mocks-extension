5c6ad5d7ab6b6e90386cc87c5a8194de3c1a79bf
bogdan.mitroiu <bogdan.mitroiu@stud.etti.upb.ro>
2021-09-24 18:57:53 +0300

    Init setup

---
+++ src/main/java/com/example/JunitPlayGround/junit5playground/Junit5PlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.example.JunitPlayGround.junit5playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Junit5PlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Junit5PlaygroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/JunitPlayGround/junit5playground/Junit5PlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JunitPlayGround.junit5playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Junit5PlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
